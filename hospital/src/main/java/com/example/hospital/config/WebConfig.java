794374a66ac5c12d05ea1c39b73308e1b9a0f9a6
Harshini-1305 <157449463+Harshini-1305@users.noreply.github.com>
2026-04-15 14:46:19 +0530

    Update WebConfig.java

--- hospital/src/main/java/com/example/hospital/config/WebConfig.java
+++ hospital/src/main/java/com/example/hospital/config/WebConfig.java
@@ -2,7 +2,8 @@ package com.example.hospital.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.*;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
@@ -15,10 +16,19 @@ public class WebConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
+  
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:5173")); // React URL
+
+        
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://hospital-management-system-6-f8om.onrender.com"
+        ));
+
+      
+
         config.setAllowedHeaders(List.of("*"));
-        config.setAllowedMethods(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
